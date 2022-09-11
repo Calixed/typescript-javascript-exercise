cd8bb18cd2e388bcaeb9cf17d9207c54b0f48c15
Vince Nery II <58459176+Calixed@users.noreply.github.com>
2022-09-12 01:44:39 +0800

    Added, adding how to use,declare,implement function as a types

--- app.ts
+++ app.ts
@@ -8,5 +8,18 @@ const printResult = (num: number): void => {
     console.log('Result: '+ num); //returns a void -> returns a undefined, once ts is compiled and run
 }
 
+// declared a variable as a function
+// let combineValues: Function;
+
+// specify how the parameter should be by the TYPES
+// combineValues() -> expect two arguements in the invokement
+let combineValues: (a: number, b:number) => number; // similar to Overloading I THINK? => return a number
+
+combineValues = add;
+combineValues = printResult; // returns a undefined -> since we did note state how the function should look like
+//combineValues = 5;
+
+
+console.log(combineValues(4,5));
 // basically, we are passing the return value of add() method in printResult() method
 printResult(add(5,5));
