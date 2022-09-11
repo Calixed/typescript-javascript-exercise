c5ae0f417e1601baed7825d36d5391fe356cb7e6
Vince Nery II <58459176+Calixed@users.noreply.github.com>
2022-09-12 00:43:08 +0800

    Added, code shows how to declare,assigned an Enum

--- app.ts
+++ app.ts
@@ -1,2 +1,29 @@
-console.log('Hello, World');
-console.log('Why is this not working?');
+// enums declaration-> custom-type
+enum Role {
+    ADMIN, READ_ONLY, AUTHOR
+};
+
+// you can add a default behaivor
+// you can also assign values but not restricted to mix different types
+// to all identifiers
+// NOTE: by default; incrementing at numbers starting at zero
+enum Popularity {
+    LOW = 10, MID = 20, HIGH = 'Hot'
+};
+// basically there value is 0,1,2
+// NOTE: Enums with all-uppercase values but that's not a "must-do"
+const person = {
+    name: 'John',
+    age: 30,
+    hobbies: ['coding', 'cooking'],
+    role: Role.ADMIN,
+    Popularity: Popularity.MID
+};
+
+if(person.role == Role.AUTHOR){
+    console.log("This person is an author")
+}
+
+// run this command to observe
+// tsc app.ts -> observe the compiled code and you can see how it is declared in js
+export {}
