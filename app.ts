13d994169464a6f4046c5b68f1762989e780ed36
Vince Nery II <58459176+Calixed@users.noreply.github.com>
2022-09-12 01:37:33 +0800

    Added, code shows how to explicit a function to return a type

--- app.ts
+++ app.ts
@@ -1,2 +1,12 @@
-console.log('Hello, World');
-console.log('Why is this not working?');
+// we explicit enforced to return a number
+const add = (n1: number, n2: number):number => {
+    return n1 + n2;
+}
+// we can have <undefined> as a type
+const printResult = (num: number): void => {
+    // what is the return here?
+    console.log('Result: '+ num); //returns a void -> returns a undefined, once ts is compiled and run
+}
+
+// basically, we are passing the return value of add() method in printResult() method
+printResult(add(5,5));
