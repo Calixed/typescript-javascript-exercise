56991e06dc4469dc4bc9e69e2b5525801d61dfff
Vince Nery II <58459176+Calixed@users.noreply.github.com>
2022-09-11 20:49:08 +0800

    Added, basics of array types

--- app.ts
+++ app.ts
@@ -1,2 +1,24 @@
-console.log('Hello, World');
-console.log('Why is this not working?');
+const person = {
+    name: 'John',
+    age: 30,
+    hobbies: ['coding', 'cooking']
+};
+
+// you can also explicit add types in an array
+
+let pets: string[]
+pets = ['pug']; // mixed types in an array doesn't support by typescript
+
+// if you want to use any types for your elements use": any[]"
+let faveThings: any[]; // flexibility but dumps all typescript benefit
+faveThings = ['John',23,'exercise']
+
+
+console.log(person.name)
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toLocaleUpperCase()); // typescript detects hobbies is string[]
+    // therefore, typescript can give us methods to manipulate hobby since it knows its a string from
+    // person.hobbies[]
+}
+
