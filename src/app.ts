4aed4627948ab5c156f1c504029d4c7c618f6073
Vince Nery II <58459176+Calixed@users.noreply.github.com>
2022-09-12 13:12:42 +0800

    Added, implementing inheritance
    How to implement and super() method is in the constructor are required

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,11 @@ class Department{
     // adding access modifiers
     private employees: string[] =[];
 
-    // constructor using a this keyword to refer the local variable
-    // using short hands for initialzing
-    constructor(private id: string, private name: string){ // initializing a variable inside of the parameter in the constructor
-        this.id = id;
-        this.name = name;
+    // Adding a readonly modifier -> disable writing after initialization //,private readonly myKey: string
+    constructor(private id: string, private name: string ){ // initializing a variable inside of the parameter in the constructor
+        // this.id = id; // using short-hand; theres no need for this manual initialization
+        // this.name = name;
+
     }
 
     // class method
@@ -21,6 +21,7 @@ class Department{
     }
 
     addEmployee(employee:string){
+        // this.myKey = 'asdasd'; -> error since its only readonly
         this.employees.push(employee);
     }
 
@@ -30,23 +31,56 @@ class Department{
     }
 }
 
-// this is how you create an object
-const accountLog = new Department('d1','IT department');
+// multiple inheritance is not supported
+// note: including its constructor is inherited by the parent class
+class ITDepartment extends Department{
+    // ITDepartment its own class
+    // required to add super();
+    constructor(id: string, name: string, private admin: string[]){
+        super(id, name );
+        this.admin = admin;
+    }
+}
 
-accountLog.addEmployee('John');
-accountLog.addEmployee('Doe');
+
+class AdminDepartment extends Department{
+    // ITDepartment its own class
+    // required to add super();
+    constructor(id: string, name: string, private admin: string[]){
+        super(id, 'Admin' );
+        this.admin = admin;
+    }
+    addReport(text: string){
+        console.log(`Report of the week ${text}`);
+    }
+}
+
+// creating an intance in the child class calling the super class
+const it = new ITDepartment('d2', 'Tech department', ['john','doe']);
+
+it.addEmployee('John');
+it.addEmployee('Doe');
+
+it.describe();
+it.printEmployee()
+
+// this is how you create an object
+// superclass instance
+const itPerson1 = new Department('d1','IT department');
+itPerson1.describe();
+itPerson1.addEmployee('Mike');
+
 
 // another way of populating a attribute in a class
 // since adding 'private' modifier in a attribute enforced you to proper way of accessing it
 // accountLog.employees[2] = 'Mike';
 
 
-accountLog.describe();
 
 // this another way* how you populate a class in ts/js
 const accountCopy = {
     name: "Account Copy",
-    describe: accountLog.describe
+    describe: itPerson1.describe
 };
 
 // accountCopy.describe();
